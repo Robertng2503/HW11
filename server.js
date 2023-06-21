0cefa1bc08019c17cfc3e8937fd70b02ee9e621c
Robert Nguyen <robertng.thien@gmail.com>
2023-06-21 13:43:36 -0700

    HW11

--- server.js
+++ server.js
@@ -3,16 +3,24 @@ const path = require("path");
 
 const app = express();
 
-// routes
+// MIDDLEWARES
+app.use(express.static("public"))
 
-// GET ROUTES
+// app.get("/assets/css/styles.css", (req,res) => {
+//     res.sendFile(path.join(__dirname,"./public/assets/css/styles.css" ))
+// })
 
+
+// ROUTES
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"))
 })
-
 app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname,"./public/index.html" ))
+})
+
+app.get("/api/notes",(req,res) =>{
+    res.sendFile(path.join(__dirname,"./db/db.json") )
 })
 
 
